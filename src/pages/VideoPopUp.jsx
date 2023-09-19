084ef65fe00936b07c0fffb574a9bbc3a2002ad4
Rheynalyn Madayag <rheynalynmadayag@Rheynalyns-iMac.local>
2023-09-19 10:27:59 +0800

    add changes

--- src/pages/VideoPopUp.jsx
+++ src/pages/VideoPopUp.jsx
@@ -40,7 +40,7 @@ const VideoPopUp = ({ videoUrl, imageSrc, title }) => {
           <div className="absolute inset-0 bg-gray-900 bg-opacity-75"></div>
           <div className="relative">
             {/*close button*/}
-            <button onClick={closeModal} type="button" className="absolute -right-10 -top-10 text-white bg-primary hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
+            <button onClick={closeModal} type="button" className="absolute -right-10 -top-10 text-white bg-primary hover:bg-gray-200 hover:text-gray-900 rounded-30px text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="popup-modal">
               <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
               </svg>
