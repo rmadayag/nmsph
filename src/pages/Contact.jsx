7874d038d7aff6536a9e8964fecf51b3f323ff66
Rheynalyn Madayag <rheynalynmadayag@Rheynalyns-iMac.local>
2023-09-19 10:42:54 +0800

    add changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -89,15 +89,15 @@ const Contact = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-  
+
     if (!isFormValid() || isSubmitting) {
       console.error('Form is not valid or submission is in progress');
       // Display an error message to the user or handle the validation error
       return;
     }
-  
+
     setIsSubmitting(true);
-  
+
     // Send the email using EmailJS
     emailjs
       .send(
@@ -111,7 +111,7 @@ const Contact = () => {
           console.log('Email sent successfully:', result);
           // You can display a success message or redirect the user here
           setFormSubmitted(true);
-  
+
           // Reset form data after successful submission
           setFormData({
             name: '',
@@ -119,7 +119,7 @@ const Contact = () => {
             phoneNumber: '',
             message: '',
           });
-  
+
           // Reset the formSubmitted state to allow another submission
           setTimeout(() => {
             setFormSubmitted(false);
@@ -134,149 +134,155 @@ const Contact = () => {
         setIsSubmitting(false);
       });
   };
-  
+
   return (
     <>
-    <motion.section
-    id="contact"
-      initial="initial"
-      animate="animate"
-      variants={leftVariants} // Apply left animation
-      className='bg-center bg-cover bg-banner h-[400px] lg:h-[500px] bg-no-repeat relative'>
-      <div className='w-full h-screen'>
-        <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
-          <div className='md:left-[10%] max-w-[530px] m-auto absolute p-4'>
-            <h1 className='md:text-display text-h2 font-semibold text-left text-white'>
-              Contact Us
-            </h1>
-            <p className='max-w-[600px] text-white text-14 md:text-16 py-5'>
-              Get in touch and let us kno how we can help
+      <motion.section
+        id="contact"
+        initial="initial"
+        animate="animate"
+        variants={leftVariants} // Apply left animation
+        className='bg-center bg-cover bg-banner h-[400px] lg:h-[500px] bg-no-repeat relative'>
+        <div className='w-full h-screen'>
+          <div className='absolute top-0 w-full h-full flex flex-col justify-center text-white'>
+            <div className='md:left-[10%] max-w-[530px] m-auto absolute p-4'>
+              <h1 className='md:text-display text-h2 font-semibold text-left text-white'>
+                Contact Us
+              </h1>
+              <p className='max-w-[600px] text-white text-14 md:text-16 py-5'>
+                Get in touch and let us kno how we can help
+              </p>
+            </div>
+          </div>
+        </div>
+      </motion.section>
+
+      <motion.section
+        initial="initial"
+        animate="animate"
+        variants={rightVariants} // Apply left animation
+        className='bg-white'>
+        <div className='md:py-[70px] md:px-[10%] py-5 px-[16px] grid lg:grid-row gap-10'>
+          <div className='flex flex-col h-full justify-center gap-5'>
+            <h3 className='text-h3 font-semibold text-center md:text-h2 md:text-center text-accent'> Get in touch</h3>
+            <p className='text-center md:text-center text-accent text-14 md:text-16'>
+              We Want to Here from You
             </p>
           </div>
-        </div>
-      </div>
-    </motion.section>
+          <div className='grid md:grid-cols-2 gap-10'>
+            {/* Image */}
+            <div class="relative w-full">
+              <iframe className="md:w-full w-[350px] md:h-[620px] h-[357px] md:rounded-30px rounded-15px" frameborder="" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=nms%20baguio%20city+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Calculate population in area</a></iframe>
+            </div>
 
-    <motion.section
-      initial="initial"
-      animate="animate"
-      variants={rightVariants} // Apply left animation
-      className='bg-white'>
-      <div className='md:py-[70px] md:px-[10%] py-5 px-[16px] grid lg:grid-cols-2 gap-10'>
-        <h3 className='text-h3 font-semibold text-center md:text-h2 md:text-left text-accent'>Get in touch</h3>
-        <p className='text-center md:text-center text-accent text-14 md:text-16'>
-          We Want to Here from You
-        </p>
-        {/* Left Side */}
-        <div>
-          <iframe className="md:w-full w-[350px] md:h-[620px] h-[357px] md:rounded-30px rounded-15px" frameborder="" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=nms%20baguio%20city+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/population/">Calculate population in area</a></iframe>
-        </div>
-        {/* Right Side */}
-        <div className='flex flex-col h-full justify-center gap-5'>
-          <div class="grid grid-cols-1 md:grid-cols-1 gap-5 bg-white shadow-shadow md:rounded-30px rounded-15px">
-            <form onSubmit={handleSubmit} className="w-full mx-auto p-4">
-              <div className="mb-4">
-                <input
-                  placeholder='Name'
-                  type="text"
-                  id="name"
-                  name="name"
-                  value={formData.name}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`text-accent w-full p-3 border ${validity.name ? 'border-gray-300' : 'border-red-500'
-                    } rounded-30px focus:outline-none focus:border-blue-500`}
-                />
-                {!validity.name && (
-                  <p className="text-red-500 text-sm mt-1">Please enter a valid name.</p>
-                )}
+            {/* Left Item */}
+            <div className='flex flex-col w-full'>
+              <div className="bg-bgcolor md:w-full w-full md:rounded-30px rounded-15px px-20 py-20 shadow-shadow mb-15 md:mb-0">
+
+                <form onSubmit={handleSubmit} className="w-full mx-auto p-4">
+                  <div className="mb-4">
+                    <input
+                      placeholder='Name'
+                      type="text"
+                      id="name"
+                      name="name"
+                      value={formData.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={`text-accent w-full p-3 border ${validity.name ? 'border-gray-300' : 'border-red-500'
+                        } rounded-30px focus:outline-none focus:border-blue-500`}
+                    />
+                    {!validity.name && (
+                      <p className="text-red-500 text-sm mt-1">Please enter a valid name.</p>
+                    )}
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      placeholder='Email'
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={formData.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={`text-accent w-full p-3 border ${validity.email ? 'border-gray-300' : 'border-red-500'
+                        } rounded-30px focus:outline-none focus:border-blue-500`}
+                    />
+                    {!validity.email && (
+                      <p className="text-red-500 text-sm mt-1">Please enter a valid email address.</p>
+                    )}
+                  </div>
+
+                  <div className="mb-4">
+                    <input
+                      placeholder='Phone'
+                      type="tel"
+                      id="phoneNumber"
+                      name="phoneNumber"
+                      value={formData.phoneNumber}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      className={`text-accent w-full p-3 border ${validity.phoneNumber ? 'border-gray-300' : 'border-red-500'
+                        } rounded-30px focus:outline-none focus:border-blue-500`}
+                    />
+                    {!validity.phoneNumber && (
+                      <p className="text-red-500 text-sm mt-1">Please enter a valid phone number.</p>
+                    )}
+                  </div>
+
+                  <div className="mb-4">
+                    <textarea
+                      placeholder='Message'
+                      id="message"
+                      name="message"
+                      value={formData.message}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      rows="7"
+                      className={`text-accent w-full p-3 border  ${validity.message ? 'border-gray-300' : 'border-red-500'
+                        } rounded-30px focus:outline-none focus:border-blue-500`}
+                    />
+                    {!validity.message && (
+                      <p className="text-red-500 text-sm mt-1">Please enter a message.</p>
+                    )}
+                  </div>
+
+                  <div className="mb-4">
+                    <ReCAPTCHA
+                      ref={reCaptchaRef}
+                      sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
+                      onChange={handleRecaptchaChange}
+                    />
+                    {!validity.recaptcha && (
+                      <p className="text-red-500 text-sm mt-1">Please complete the reCAPTCHA.</p>
+                    )}
+                  </div>
+
+                  {formSubmitted && (
+                    <div className="text-green-500 text-sm my-5">
+                      Thank you for your submission!
+                    </div>
+                  )}
+
+                  <div className="text-left">
+                    <button
+                      type="submit"
+                      className={`rounded-30px bg-primary text-white font-semibold text-14 md:text-16 w-sm-button h-sm-button md:w-lg-button md:h-lg-button hover:bg-primary/80 ${!isFormValid() || isSubmitting || formSubmitted ? 'cursor-not-allowed opacity-50' : ''
+                        }`}
+                      disabled={!isFormValid() || isSubmitting || formSubmitted}
+                    >
+                      {isSubmitting ? 'Submitting...' : formSubmitted ? 'Submitted' : 'Submit'}
+                    </button>
+                  </div>
+                </form>
               </div>
-
-              <div className="mb-4">
-                <input
-                  placeholder='Email'
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={formData.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`text-accent w-full p-3 border ${validity.email ? 'border-gray-300' : 'border-red-500'
-                    } rounded-30px focus:outline-none focus:border-blue-500`}
-                />
-                {!validity.email && (
-                  <p className="text-red-500 text-sm mt-1">Please enter a valid email address.</p>
-                )}
-              </div>
-
-              <div className="mb-4">
-                <input
-                  placeholder='Phone'
-                  type="tel"
-                  id="phoneNumber"
-                  name="phoneNumber"
-                  value={formData.phoneNumber}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  className={`text-accent w-full p-3 border ${validity.phoneNumber ? 'border-gray-300' : 'border-red-500'
-                    } rounded-30px focus:outline-none focus:border-blue-500`}
-                />
-                {!validity.phoneNumber && (
-                  <p className="text-red-500 text-sm mt-1">Please enter a valid phone number.</p>
-                )}
-              </div>
-
-              <div className="mb-4">
-                <textarea
-                  placeholder='Message'
-                  id="message"
-                  name="message"
-                  value={formData.message}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  rows="7"
-                  className={`text-accent w-full p-3 border  ${validity.message ? 'border-gray-300' : 'border-red-500'
-                    } rounded-30px focus:outline-none focus:border-blue-500`}
-                />
-                {!validity.message && (
-                  <p className="text-red-500 text-sm mt-1">Please enter a message.</p>
-                )}
-              </div>
-
-              <div className="mb-4">
-                <ReCAPTCHA
-                  ref={reCaptchaRef}
-                  sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"
-                  onChange={handleRecaptchaChange}
-                />
-                {!validity.recaptcha && (
-                  <p className="text-red-500 text-sm mt-1">Please complete the reCAPTCHA.</p>
-                )}
-              </div>
-
-              {formSubmitted && (
-                <div className="text-green-500 text-sm my-5">
-                  Thank you for your submission!
-                </div>
-              )}
-
-              <div className="text-left">
-                <button
-                  type="submit"
-                  className={`rounded-30px bg-primary text-white font-semibold text-14 md:text-16 w-sm-button h-sm-button md:w-lg-button md:h-lg-button ${!isFormValid() || isSubmitting || formSubmitted ? 'cursor-not-allowed' : ''
-                    }`}
-                  disabled={!isFormValid() || isSubmitting || formSubmitted}
-                >
-                  {isSubmitting ? 'Submitting...' : formSubmitted ? 'Submitted' : 'Submit'}
-                </button>
-              </div>
-            </form>
-           
+            </div>
           </div>
         </div>
-      </div>
-    </motion.section>
-  </>
+      </motion.section>
+
+    </>
   );
 };
 
